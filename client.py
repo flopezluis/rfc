3eede24e4f61914db32e7c435d1f2eed691620dd
Felix <hola@flopezluis.es>
2014-11-03 14:41:35 +0000

    remove useless code

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ context = zmq.Context()
 print "Connecting to server..."
 socket = context.socket(zmq.REQ)
 socket.connect ("tcp://localhost:7313")
-socket.connect ("tcp://localhost:7313")
-socket.send ("Hello")
+socket.send("Hello")
 message = socket.recv()
 print "Received reply %s" %message
--- server.py
+++ server.py
@@ -5,7 +5,5 @@ import sys
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:7313")
-
-while True:
-    message = socket.recv()
-    print "Received request: ", message
+message = socket.recv()
+print "Received request: ", message
