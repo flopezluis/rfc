42d08dc803281184c4362ab1f47ef4ebf328b185
Felix <hola@flopezluis.es>
2014-11-03 14:45:03 +0000

    added reply from server

--- server.py
+++ server.py
@@ -5,5 +5,10 @@ import sys
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:7313")
-message = socket.recv()
-print "Received request: ", message
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print "Received request: ", message
+    time.sleep (1)
+    socket.send("Hello there")
